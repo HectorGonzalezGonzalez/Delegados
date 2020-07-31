7cfcd7cb030603d0b7d4a495f5225a512682a471
HectorGonzalezGonzalez <37567557+HectorGonzalezGonzalez@users.noreply.github.com>
2020-07-31 18:56:40 -0500

    Agregar archivos de proyecto.

---
+++ Ejemplo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using static Delegados.Program;
+
+namespace Delegados
+{
+    public class Ejemplo
+    {
+        public List<int> NumerosPares(List<int> Lst, myDelegate objeDelegado)
+        {
+            List<int> LstNumerosEnteros = new List<int>();
+            foreach (int item in Lst)
+            {
+                //Console.WriteLine($"Elemento: {item}");
+                objeDelegado(item.ToString());
+                if (item % 2 == 0)
+                {
+                    LstNumerosEnteros.Add(item);
+                    Thread.Sleep(4000);
+                }
+            }
+            return LstNumerosEnteros;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Delegados
+{
+   public class Program
+    {
+       public delegate void myDelegate(string msj);
+        static void Main(string[] args)
+        {
+
+
+
+
+            myDelegate objeDelegado;
+            objeDelegado = MostrarMensaje;
+            //objeDelegado += MostrarMensaje;
+            //objeDelegado("hola");
+            //objeDelegado = MostrarMensajeDos;
+            //objeDelegado("mensaje dos");
+
+
+            /*Ejemplo De Delegados Real*/
+            Ejemplo objE = new Ejemplo();
+            List<int> LstNumeros = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            List<int> LstNumerosPares = objE.NumerosPares(LstNumeros, objeDelegado);
+            Console.WriteLine("Numeros pares: \n");
+            foreach (int item in LstNumerosPares)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+        public static void MostrarMensaje(string msj)
+        {
+            Console.WriteLine(msj);
+        }
+        public static void MostrarMensajeDos(string msjDos)
+        {
+            Console.WriteLine(msjDos);
+        }
+
+    }
+}
